bccf745a5a805539dc7cc3f396d837951bd0c116
Axhu99 <xhuliodreni@icloud.com>
2024-04-06 16:50:24 +0200

    fix date

--- src/components/words/WordCard.vue
+++ src/components/words/WordCard.vue
@@ -13,13 +13,16 @@ export default {
             let day = date.getDate();
             let month = date.getMonth() + 1;
             const year = date.getFullYear();
-            const hours = date.getHours();
-            const minutes = date.getMinutes();
+            let hours = date.getHours();
+            let minutes = date.getMinutes();
 
             if (day < 10) day = '0' + day;
             if (month < 10) month = '0' + month;
+            if (hours < 10) hours = '0' + hours;
+            if (minutes < 10) minutes = '0' + minutes;
 
-            return `${day} /${month}/${year} alle ${hours}:${minutes}`;
+
+            return `${day}/${month}/${year} alle ${hours}:${minutes}`;
         }
     }
 };
